468200acbe7976649e8805d4a935646f5edf349e
Zai-Kun <122824602+Zai-Kun@users.noreply.github.com>
2024-12-07 19:31:51 +0500

    small update

--- src/main.rs
+++ src/main.rs
@@ -8,21 +8,22 @@ mod day_7;
 
 
 fn main() {
-    // day_1::part_1::run().unwrap();
-    // day_1::part_2::run().unwrap();
+    day_1::part_1::run().unwrap();
+    day_1::part_2::run().unwrap();
 
-    // day_2::part_1::run().unwrap();
-    // day_2::part_2::run().unwrap();
+    day_2::part_1::run().unwrap();
+    day_2::part_2::run().unwrap();
 
-    // day_3::part_1::run().unwrap();
-    // day_3::part_2::run().unwrap();
+    day_3::part_1::run().unwrap();
+    day_3::part_2::run().unwrap();
 
-    // day_4::part_1::run().unwrap();
-    // day_4::part_2::run().unwrap();
+    day_4::part_1::run().unwrap();
+    day_4::part_2::run().unwrap();
 
-    // day_5::part_1_n_2::run().unwrap();
+    day_5::part_1_n_2::run().unwrap();
 
-    // day_6::part_1_n_2::run().unwrap();
+    day_6::part_1_n_2::run().unwrap();
+
     day_7::part_1::run().unwrap();
     day_7::part_2::run().unwrap();
 }
