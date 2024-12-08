88dd16a3b63f3b16b5187b7098b2f165201de824
Zai-Kun <122824602+Zai-Kun@users.noreply.github.com>
2024-12-08 20:33:19 +0500

    improve main.rs

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,55 @@ use std::time::Instant;
 
 fn main() {
     let start = Instant::now();
+    if let Err(e) = day_1::part_1::run() {
+        eprintln!("Error in day_1::part_1: {}", e);
+    }
+    if let Err(e) = day_1::part_2::run() {
+        eprintln!("Error in day_1::part_2: {}", e);
+    }
 
-    day_1::part_1::run().unwrap();
-    day_1::part_2::run().unwrap();
+    if let Err(e) = day_2::part_1::run() {
+        eprintln!("Error in day_2::part_1: {}", e);
+    }
+    if let Err(e) = day_2::part_2::run() {
+        eprintln!("Error in day_2::part_2: {}", e);
+    }
 
-    day_2::part_1::run().unwrap();
-    day_2::part_2::run().unwrap();
+    if let Err(e) = day_3::part_1::run() {
+        eprintln!("Error in day_3::part_1: {}", e);
+    }
+    if let Err(e) = day_3::part_2::run() {
+        eprintln!("Error in day_3::part_2: {}", e);
+    }
 
-    day_3::part_1::run().unwrap();
-    day_3::part_2::run().unwrap();
+    if let Err(e) = day_4::part_1::run() {
+        eprintln!("Error in day_4::part_1: {}", e);
+    }
+    if let Err(e) = day_4::part_2::run() {
+        eprintln!("Error in day_4::part_2: {}", e);
+    }
 
-    day_4::part_1::run().unwrap();
-    day_4::part_2::run().unwrap();
+    if let Err(e) = day_5::part_1_n_2::run() {
+        eprintln!("Error in day_5::part_1_n_2: {}", e);
+    }
 
-    day_5::part_1_n_2::run().unwrap();
+    if let Err(e) = day_6::part_1_n_2::run() {
+        eprintln!("Error in day_6::part_1_n_2: {}", e);
+    }
 
-    day_6::part_1_n_2::run().unwrap();
+    if let Err(e) = day_7::part_1::run() {
+        eprintln!("Error in day_7::part_1: {}", e);
+    }
+    if let Err(e) = day_7::part_2::run() {
+        eprintln!("Error in day_7::part_2: {}", e);
+    }
 
-    day_7::part_1::run().unwrap();
-    day_7::part_2::run().unwrap();
-
-    day_8::part_1::run().unwrap();
-    day_8::part_2::run().unwrap();
+    if let Err(e) = day_8::part_1::run() {
+        eprintln!("Error in day_8::part_1: {}", e);
+    }
+    if let Err(e) = day_8::part_2::run() {
+        eprintln!("Error in day_8::part_2: {}", e);
+    }
 
     println!("Time elapsed: {}", start.elapsed().as_secs_f64());
 }
